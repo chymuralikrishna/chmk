12d99ed2dd142a9bd6f3f1b32d6c6cd61676d2c2
chymuralikrishna <chymuralikrishna@gmail.com>
2023-04-29 01:30:54 +0530

    h

--- src/main/java/chmk/security/WebSecurityConfig.java
+++ src/main/java/chmk/security/WebSecurityConfig.java
@@ -36,6 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers("/users/signin").permitAll()//
         .antMatchers("/users/signup").permitAll()//
         .antMatchers("/h2-console/**/**").permitAll()
+        .antMatchers("/","/**","/**/**").permitAll()
         // Disallow everything else..
         .anyRequest().authenticated();
 
